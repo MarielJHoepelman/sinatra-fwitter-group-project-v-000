1dcf1140e170e31e7bde793dd399828abf042ada
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-24 19:05:13 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,25 +1,22 @@
+# ENV['SINATRA_ENV'] ||= "development"
+#
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+#
+# ActiveRecord::Base.establish_connection(
+#   :adapter => "sqlite3",
+#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# )
+#
+# require_all 'app'
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require_all 'app'
-
-
-require 'bundler/setup'
 Bundler.require
 
 configure :development do
-  ENV['SINATRA_ENV'] ||= "development"
-  require 'bundler/setup'
-
-  Bundler.require(:default, ENV['SINATRA_ENV'])
-
   ActiveRecord::Base.establish_connection(
     :adapter => "sqlite3",
     :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
@@ -38,3 +35,5 @@ configure :production do
    :encoding => 'utf8'
   )
 end
+
+require_all 'app'
