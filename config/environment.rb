a6b0039a9981587deb4c848c7721029cef1738e5
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-11-24 19:09:57 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,21 +1,7 @@
-# ENV['SINATRA_ENV'] ||= "development"
-#
-# require 'bundler/setup'
-# Bundler.require(:default, ENV['SINATRA_ENV'])
-#
-# ActiveRecord::Base.establish_connection(
-#   :adapter => "sqlite3",
-#   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-# )
-#
-# require_all 'app'
-
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
-# Bundler.require
 
 configure :development do
   ActiveRecord::Base.establish_connection(
